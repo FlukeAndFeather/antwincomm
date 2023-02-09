11c5ade3a7a1e0f94e87afab1d757587d3ab9469
Max Czapanskiy <maxczap@stanford.edu>
2023-02-09 10:56:49 -0800

    Fixed projection and finished implementing basemap

--- R/sp.R
+++ R/sp.R
@@ -1,6 +1,11 @@
-# Use WGS 84 / Antarctic Polar Stereographic for mapping
+# Use WGS 84 / Antarctic Polar Stereographic for mapping, recentered on ant_lims()
 ant_proj <- function() {
-  sf::st_crs("EPSG:3031")
+  lat_ts <- (ant_lims()["ymin"] + ant_lims()["ymax"]) / 2
+  lon_0 <- (ant_lims()["xmin"] + ant_lims()["xmax"]) / 2
+  glue::glue(
+    "+proj=stere +lat_0=-90 +lat_ts={lat_ts} +lon_0={lon_0} +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +type=crs"
+  ) %>%
+  sf::st_crs()
 }
 
 ant_lims <- function() {
@@ -26,9 +31,7 @@ create_ant_sf <- function(ne_dir, limits = ant_lims()) {
 
 ant_raster_template <- function(limits = ant_lims(), res = 5e4) {
   lims_prj <- sf::st_bbox(limits, crs = "EPSG:4326") %>%
-    sf::st_as_sfc() %>%
-    sf::st_transform(ant_proj()) %>%
-    sf::st_bbox()
+    project_bbox()
   raster::raster(xmn = lims_prj["xmin"],
                  xmx = lims_prj["xmax"],
                  ymn = lims_prj["ymin"],
@@ -58,11 +61,11 @@ latlon_to_sf <- function(df, coords = c("x", "y")) {
                crs = sp::CRS("+proj=longlat +datum=WGS84"))
 }
 
-ant_basemap <- function() {
-  map_lim <- sf::st_bbox(ant_lims(), crs = "EPSG:4326") %>%
-    sf::st_as_sfc() %>%
-    sf::st_transform(ant_proj()) %>%
-    sf::st_bbox()
+ant_basemap <- function(map_lim = NULL) {
+  if (is.null(map_lim)) {
+    map_lim <- sf::st_bbox(ant_lims(), crs = "EPSG:4326") %>%
+      project_bbox()
+  }
 
   ggplot() +
     geom_sf(data = tar_read("ant_sf", store = here::here("_targets"))) +
@@ -72,4 +75,24 @@ ant_basemap <- function() {
              crs = ant_proj()) +
     theme_minimal() +
     theme(axis.title = element_blank())
+}
+
+project_bbox <- function(x) {
+  x %>%
+    sf::st_as_sfc(crs = "EPSG:4326") %>%
+    sf::st_transform(ant_proj()) %>%
+    sf::st_bbox()
+}
+
+expand_bbox <- function(x, factor) {
+  result <- x
+  xrng <- x["xmax"] - x["xmin"]
+  x0 <- x["xmin"] + xrng / 2
+  yrng <- x["ymax"] - x["ymin"]
+  y0 <- x["ymin"] + yrng / 2
+  result["xmin"] <- x0 - xrng * factor / 2
+  result["xmax"] <- x0 + xrng * factor / 2
+  result["ymin"] <- y0 - yrng * factor / 2
+  result["ymax"] <- y0 + yrng * factor / 2
+  result
 }
