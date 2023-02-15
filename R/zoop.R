1082e10a8f42e239dc2e1e60560e456a5e2a06bd
Max Czapanskiy <maxczap@stanford.edu>
2023-02-15 10:57:01 -0800

    Add zoop/physics maps

--- R/zoop.R
+++ R/zoop.R
@@ -2,7 +2,12 @@ read_zoop <- function(zoop_path) {
   readxl::read_excel(zoop_path, sheet = 1) %>%
     mutate(time.of.day = factor(time.of.day,
                                 levels = c("D", "T", "N"),
-                                labels = c("Day", "Twilight", "Night"))) %>%
+                                labels = c("Day", "Twilight", "Night")),
+           across(c(zuml_m,
+                    starts_with("avg"),
+                    starts_with("Integ"),
+                    Net.ice.percent),
+                  parse_number)) %>%
     select(-57) %>%
     filter(leg == "W")
 }
