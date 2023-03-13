d4c7207011d52a0c28dbf9f566ec232308b224ef
Max Czapanskiy <maxczap@stanford.edu>
2023-03-13 14:55:14 -0700

    render reports

--- R/wamlr.R
+++ R/wamlr.R
@@ -1,5 +1,5 @@
 check_wamlr <- function(wamlr_path) {
-  if (!dir.exists(path))
+  if (!dir.exists(wamlr_path))
     stop(str_glue("WAMLR directory not found at {wamlr_path}. Follow the ",
                   "instructions in the README for downloading these data."))
   wamlr_path
