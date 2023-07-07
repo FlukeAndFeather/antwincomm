badc06899c3bb732f895a85e8db579375ed3f6a2
Max Czapanskiy <maxczap@stanford.edu>
2023-07-07 12:56:57 -0300

    This ice concentration data seems to work!

--- scratch/ice_conc.R
+++ scratch/ice_conc.R
@@ -1,57 +1,25 @@
-library(ncdf4)
 library(terra)
 library(tidyterra)
 library(tidyverse)
 
+seaice <- "~/Downloads/Aug sea ice concentration/" %>%
+  dir(full.names = TRUE) %>%
+  map(rast) %>%
+  rast()
+set.names(seaice, as.character(2012:2016))
+
 proj_ext <- ext(-67, -50, -68, -55) %>%
   project(from = "epsg:4326",
-          to = "epsg:3412")
-url <- str_glue("
-https://polarwatch.noaa.gov/erddap/griddap/nsidcG02202v4nhmday.csv?cdr_seaice_conc_monthly%5B(2012-08-01T00:00:00Z):12:(2016-08-01T00:00:00Z)%5D%5B({round(proj_ext[3])}):1:({round(proj_ext[4])})%5D%5B({round(proj_ext[1])}):1:({round(proj_ext[2])})%5D
-")
-
-seaice_df <- read_csv("~/Downloads/nsidcG02202v4nhmday_16d4_f5d0_af8c.csv",
-                      col_names = c("time",
-                                    "y",
-                                    "x",
-                                    "seaice_conc"),
-                      col_types = "Tddd",
-                      skip = 2) %>%
-  mutate(year = lubridate::year(time))
-seaice_rast <- seaice_df %>%
-  split()
-rast(seaice_df,
-                    type = "xy",
-                    crs = "epsg:3412",
-                    extent = proj_ext)
-
-ice_nc <- nc_open("~/Downloads/nsidcG02202v4shmday_df31_2bad_59c8_U1688308936040.nc")
-ice_conc_arr <- ncvar_get(ice_nc, "cdr_seaice_conc_monthly") %>%
-  # NETcdf stores values reversed along x-axis
-  apply(3, pracma::fliplr, simplify = FALSE) %>%
-  simplify2array()
-ice_conc_ext <- ext(
-  min(ncvar_get(ice_nc, "xgrid")),
-  max(ncvar_get(ice_nc, "xgrid")),
-  min(ncvar_get(ice_nc, "ygrid")),
-  max(ncvar_get(ice_nc, "ygrid"))
-)
-ice_conc <- rast(
-  ice_conc_arr,
-  crs = "epsg:3412",
-  ice_conc_ext
-)
-
+          to = crs(seaice))
+npen_seaice <- crop(seaice, proj_ext)
 ice_contour <- map(
-  seq(nlyr(ice_conc)),
-  ~ as.contour(ice_conc[[.x]], levels = 0.65)
+  seq(nlyr(npen_seaice)),
+  ~ as.contour(npen_seaice[[.x]], levels = 700)
 ) %>%
   vect() %>%
-  mutate(Year = 2012:2016)
-
-ice_conc_df <- as.data.frame(ice_conc)
-
-plot(ice_conc)
+  mutate(Year = 2012:2016,
+         ice_conc = level / 1000)
+plot(ice_contour)
 
 ggplot() +
   geom_spatraster(data = ice_conc) +
