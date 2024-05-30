c85b3e04f4333bf20bc7c7a5ce8262d5f2738ed2
Max Czapanskiy <maxczapanskiy@gmail.com>
2024-05-30 12:42:07 -0700

    begin writing paper: intro

---
+++ analysis/scripts/01_download_data.R
@@ -0,0 +1,8 @@
+# Download dataset zip from USAP Data Center
+download.file(
+  "https://www.usap-dc.org/dataset/usap-dc/601795/2024-05-28T19:16:39.2Z/AntPeninsula_winter_marine_data.zip",
+  "analysis/data/raw_data/AntPeninsula_winter_marine_data.zip"
+)
+
+# If necessary to do manually, visit https://doi.org/10.15784/601795 and
+# download AntPeninsula_winter_marine_data.zip to analysis/data/raw_data.
