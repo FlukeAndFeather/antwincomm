e9f0c2c658af5589b1d89619cc486dfec5a1b790
Max Czapanskiy <maxczapanskiy@gmail.com>
2024-05-30 11:10:26 -0700

    Set up research compendium

--- R/wamlr.R
+++
@@ -1,6 +0,0 @@
-check_wamlr <- function(wamlr_path) {
-  if (!dir.exists(wamlr_path))
-    stop(str_glue("WAMLR directory not found at {wamlr_path}. Follow the ",
-                  "instructions in the README for downloading these data."))
-  wamlr_path
-}
